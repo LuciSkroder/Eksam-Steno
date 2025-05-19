a7f42e3411371c50025abd6114acda7eb6a39400
imikkelsen <ingibjorgm1997@gmail.com>
2025-05-20 00:25:23 +0200

    fjúú!! prøve at få hover at virke

--- javascript/ultralyd.js
+++ javascript/ultralyd.js
@@ -3,18 +3,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dommie1 = document.querySelector('.dommie1');
     const container = document.getElementById('dommie-body');
-  
+
     container.addEventListener('mousemove', (e) => {
-      const rect = container.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
-  
-      dommie1.classList.add('masked');
-      dommie1.style.clipPath = `circle(100px at ${x}px ${y}px)`;
+        const rect = container.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        dommie1.classList.add('masked');
+        dommie1.style.setProperty('--x', `${x}px`);
+        dommie1.style.setProperty('--y', `${y}px`);
     });
-  
+
     container.addEventListener('mouseleave', () => {
-      dommie1.classList.remove('masked');
-      dommie1.style.clipPath = `circle(0px at 0px 0px)`;
+        dommie1.classList.remove('masked');
     });
-  });
+});
