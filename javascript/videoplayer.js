3978ea0e52ab1e3b37e2cc141ffb37968c84df7b
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-05-21 11:34:47 +0200

    js start, eventListener

--- javascript/videoplayer.js
+++ javascript/videoplayer.js
@@ -1,2 +1,17 @@
 "use strict";
 
+const videoOne = document.getElementById("video-1")
+const videoTwo = document.getElementById("video-2")
+const videoThree = document.getElementById("video-3")
+const videoFour = document.getElementById("video-4")
+
+videoOne.addEventListener("touchstart")
+videoTwo.addEventListener("touchstart")
+videoThree.addEventListener("touchstart")
+videoFour.addEventListener("touchstart")
+
+videoOne.addEventListener("click")
+videoTwo.addEventListener("click")
+videoThree.addEventListener("click")
+videoFour.addEventListener("click")
+
