d795abd9e6226893b04afae2b36112f3264c4004
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-05-21 12:00:55 +0200

    start på open video function

--- javascript/videoplayer.js
+++ javascript/videoplayer.js
@@ -4,14 +4,22 @@ const videoOne = document.getElementById("video-1")
 const videoTwo = document.getElementById("video-2")
 const videoThree = document.getElementById("video-3")
 const videoFour = document.getElementById("video-4")
+const videoContainer = document.getElementById("video-spiller")
+const playVideo = document.getElementById("current-video")
+const playAudio = document.getElementById("current-audio")
 
-videoOne.addEventListener("touchstart")
-videoTwo.addEventListener("touchstart")
-videoThree.addEventListener("touchstart")
-videoFour.addEventListener("touchstart")
+function openVideo(){
+    videoContainer.classList.remove("hidden")
+    videoContainer.classList.add("show")
+}
 
-videoOne.addEventListener("click")
-videoTwo.addEventListener("click")
-videoThree.addEventListener("click")
-videoFour.addEventListener("click")
 
+videoOne.addEventListener("touchstart", openVideo)
+videoTwo.addEventListener("touchstart", openVideo)
+videoThree.addEventListener("touchstart", openVideo)
+videoFour.addEventListener("touchstart", openVideo)
+
+videoOne.addEventListener("click", openVideo)
+videoTwo.addEventListener("click", openVideo)
+videoThree.addEventListener("click", openVideo)
+videoFour.addEventListener("click", openVideo)
